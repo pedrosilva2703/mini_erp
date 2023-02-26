21907d1a3ce064809ad499012ed7f4c5f6612bd8
pedrosilva2703 <up201806526@fe.up.pt>
2023-02-26 17:20:46 +0000

    Menu button selection fixed
    Using 2 different classes on css

--- src/main/java/com/example/minierp/controllers/LayoutController.java
+++ src/main/java/com/example/minierp/controllers/LayoutController.java
@@ -3,9 +3,10 @@ package com.example.minierp.controllers;
 import com.example.minierp.HelloApplication;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
+
 
 import java.io.IOException;
 
@@ -13,10 +14,69 @@ public class LayoutController {
     @FXML
     private BorderPane mainPane;
 
+    @FXML private Button scheduleButton;
+    @FXML private Button clientsButton;
+    @FXML private Button clientOrdersButton;
+    @FXML private Button suppliersButton;
+    @FXML private Button supplierOrdersButton;
+    @FXML private Button settingsButton;
+    @FXML private Button aboutButton;
+
+
+    @FXML
+    private void onScheduleButtonClick(){
+        refreshButtonStates(scheduleButton);
+    }
+    @FXML
+    private void onClientsButtonClick(){
+        refreshButtonStates(clientsButton);
+    }
+    @FXML
+    private void onClientOrdersButtonClick(){
+        refreshButtonStates(clientOrdersButton);
+    }
+    @FXML
+    private void onSuppliersButtonClick(){
+        refreshButtonStates(suppliersButton);
+    }
+    @FXML
+    private void onSupplierOrdersButtonClick(){
+        refreshButtonStates(supplierOrdersButton);
+    }
     @FXML
     private void onSettingsButtonClick(){
         loadPage("Settings");
+        refreshButtonStates(settingsButton);
     }
+    @FXML
+    private void onAboutButtonClick(){
+        loadPage("Settings");
+        refreshButtonStates(aboutButton);
+    }
+
+    private void unselectButton(Button b){
+        b.getStyleClass().remove("menu-app-button-selected");
+        b.getStyleClass().add("menu-app-button");
+    }
+    private void selectButton(Button b){
+        b.getStyleClass().remove("menu-app-button");
+        b.getStyleClass().add("menu-app-button-selected");
+    }
+
+    private void refreshButtonStates(Button clickedButton){
+        // Unselect all buttons
+        unselectButton(scheduleButton);
+        unselectButton(clientsButton);
+        unselectButton(clientOrdersButton);
+        unselectButton(suppliersButton);
+        unselectButton(supplierOrdersButton);
+        unselectButton(settingsButton);
+        unselectButton(aboutButton);
+
+        // Select clicked button
+        selectButton(clickedButton);
+    }
+
 
     private void loadPage(String page){
         try {
